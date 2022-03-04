a3f37868a8084f93119eaf0b45cec9b4a2892f0e
Sungil Kang <oniono@naver.com>
2022-03-04 16:06:01 +0900

    changed module name

---
+++ bin/freq-serverless-api.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { FreqServerlessApiStack } from '../lib/freq-serverless-api-stack';
+
+const app = new cdk.App();
+new FreqServerlessApiStack(app, 'FreqServerlessApiStack',
+    {
+        env: {
+            account: process.env.AWS_TARGET_ACCOUNT_ID
+        }
+    });
---
+++ lib/freq-serverless-api-stack.ts
@@ -0,0 +1,19 @@
+import * as cdk from '@aws-cdk/core';
+import { Cognito } from './cognito.construct';
+import { ApiConstruct } from './api.construct';
+
+export class FreqServerlessApiStack extends cdk.Stack {
+  // Apply default config here
+  config = { hostedAuthDomainPrefix: 'my-auth-1591780305' };
+
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const cognitoResources = new Cognito(this, 'Cognito', {
+      hostedAuthDomainPrefix: this.config.hostedAuthDomainPrefix,
+    });
+    new ApiConstruct(this, 'ApiConstruct', {
+      userPool: cognitoResources.userPool,
+    });
+  }
+}
